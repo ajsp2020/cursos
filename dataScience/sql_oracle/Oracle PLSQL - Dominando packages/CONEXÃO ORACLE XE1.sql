8c81e610f7a10fd421d3b477c9cc378fb3151cf7
Antonio João da Silva Pereira <antonio@MacBook-Air-de-Antonio.local>
2021-09-09 14:19:23 -0300

    09.09-14

---
+++ dataScience/sql_oracle/Oracle PLSQL - Dominando packages/CONEXÃO ORACLE XE1.sql
@@ -0,0 +1,2 @@
+alter user user_dev quota unlimited on users;
+alter user user_app quota unlimited on users;
---
+++ programacao/java/cursoJavaCompleto/estruturasRepetittivas/src/estruturasRepetittivas/WhileTemperatura.java
@@ -0,0 +1,31 @@
+package estruturasRepetittivas;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class WhileTemperatura {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		char resp = 's';
+		
+		while (resp != 'n') {
+			
+			System.out.println("Digite a temperatura em Celsius: ");
+			double C = sc.nextDouble();
+			double F = 9.0 * C / 5.0 + 32.0;
+			System.out.printf("Equivalente em Fahrenheit: %.1f%n", F);
+			
+			System.out.print("Deseja repetir (s/n)? ");
+			resp = sc.next().charAt(0);
+			
+		}
+		
+		sc.close();
+		
+	}
+
+}
