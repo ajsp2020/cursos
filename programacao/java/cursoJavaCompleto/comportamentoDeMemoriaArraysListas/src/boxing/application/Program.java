2c1afc5307d86d47d0ee310dd440f315a99ff796
Antonio João da Silva Pereira <antonio@MacBook-Air-de-Antonio.local>
2021-09-11 11:52:01 -0300

    09.11-11

---
+++ programacao/java/cursoJavaCompleto/comportamentoDeMemoriaArraysListas/src/boxing/application/Program.java
@@ -0,0 +1,18 @@
+package boxing.application;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		int x = 20;
+		
+		Integer obj = x; // Object obj
+		
+		System.out.println(obj);
+		
+		int y = obj * 2; //(int) obj
+		
+		System.out.println(y);
+	}
+
+}
